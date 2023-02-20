e115a4d5d715a349583f735bc9090fb0a27fd208
jwalus <walusjacob@gmail.com>
2023-02-20 11:07:34 -0600

    header test

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import React from 'react';
 function App() {
   return (
     <div>
+      <header>
+        <h1>Header</h1>
+      </header>
       <h1>Test Hello World.</h1>
       <h2>Branch Test Andy</h2>
       <h2>Branch Test Jacob</h2>
