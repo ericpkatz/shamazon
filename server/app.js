4e0e0fda8a51fb592bb6d595b5086c7b4d6c022f
asorgatz <113410935+asorgatz@users.noreply.github.com>
2023-02-22 09:27:04 -0600

    server copy

--- server/app.js
+++ server/app.js
@@ -3,10 +3,9 @@ const app = express();
 const path = require('path');
 app.use(express.json());
 
-app.use('/dist', express.static(path.join(__dirname, '../dist')));
-app.use('/static', express.static(path.join(__dirname, '../static')));
 
-app.get('/', (req, res)=> res.sendFile(path.join(__dirname, '../static/index.html')));
+
+
 
 app.use('/api/auth', require('./api/auth'));
 
