c2e67000822d24b1d76c8cf61d9822a39d4c1683
asorgatz <113410935+asorgatz@users.noreply.github.com>
2023-03-08 15:12:34 -0600

    server sends web page

--- src/fetch.js
+++ src/fetch.js
@@ -2,7 +2,7 @@
 
 export const fetchProducts = async () => {
     try {
-        const response = await fetch('localhost:3000/api/products', {
+        const response = await fetch('/api/products', {
             method: 'GET'
         })
         const result = await response.json();
@@ -73,7 +73,7 @@ export const fetchCart = async (token, user_id) =>{
 
 export const fetchAllProducts = async () =>{
         try {
-            const response = await fetch('localhost:3000/api/products', {
+            const response = await fetch('/api/products', {
                 method: 'GET'
             })
             const result = await response.json();
