526add2f421f84279709ae8533b5d04e02ed8d0c
asorgatz <113410935+asorgatz@users.noreply.github.com>
2023-03-12 20:29:45 -0500

    typo fixed from last

--- server/api/cart.js
+++ server/api/cart.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cartsRouter = express.Router()
 const {getCartByUserId} = require('../db')
 
-app.get('/:userId', async (req, res) => {
+cartsRouter.get('/:userId', async (req, res) => {
     const {userId} = req.params
     const cart = await getCartByUserId({userId})
     res.send(cart)
